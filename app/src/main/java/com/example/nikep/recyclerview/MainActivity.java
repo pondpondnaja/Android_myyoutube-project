a4bf7fc7715954a28b07de27123aa4f664ca0c1c
PEtch <futayo110@gmail.com>
2018-11-10 20:01:09 +0700

    Before use Youtube Data API V3 to get Json from Youtube

--- app/src/main/java/com/example/nikep/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/nikep/recyclerview/MainActivity.java
@@ -6,11 +6,25 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     //Variable
+    private RecyclerView mRecyclerview;
+    private RecyclerViewAdapter mRecyclerviewAdapter;
+    private RequestQueue mRequestqueue;
     private ArrayList<String> mName = new ArrayList<>();
     private ArrayList<String> mImageUrl = new ArrayList<>();
     private ArrayList<String> mVideoUrl = new ArrayList<>();
@@ -27,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private void initImagebitmap(){
         Log.d(TAG, "initImagebitmap: preparing bitmap.");
 
+        mRequestqueue = Volley.newRequestQueue(this);
+        pasteJSON();
+        /*
         mImageUrl.add("https://i.pinimg.com/originals/32/d8/fe/32d8fea5ce212f8c76f894c60a909487.jpg");
         mName.add("Animegirl 1");
         mVideoUrl.add("K36sEFvFHXk");
@@ -42,12 +59,62 @@ public class MainActivity extends AppCompatActivity {
         mImageUrl.add("http://www.wallpapermaiden.com/image/2018/02/02/darling-in-the-franxx-zero-two-long-pink-hair-anime-19728.jpg");
         mName.add("Zero two");
         mVideoUrl.add("2oQffkFWK2Y");
-
-        mImageUrl.add("http://www.wallpapermaiden.com/wallpaper/20113/download/2560x1600/darling-in-the-franxx-ichigo-blue-hair-coffee.jpg");
-        mName.add("Ichigo");
-        mVideoUrl.add("q6dXsIvOl-M");
-
+        */
         initRecycleView();
+    }
+
+    private void pasteJSON(){
+        Log.d(TAG, "pasteJSON: Json pasting");
+        String url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=JP&key=AIzaSyDIS87lftBJA1lrSp4umI3t3rr6tfiyByY";
+        //String url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=JP&key="+YoutubeConfig.getApiKey();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "onResponse: JSON respond");
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("items"); //root in JSON file
+
+                            for(int i = 0; i < jsonArray.length(); i++){
+                                JSONObject item = jsonArray.getJSONObject(i);
+                                String videadd = item.getString("id");            //add Video url
+                                mVideoUrl.add(videadd);
+
+                                JSONArray jsonROW = item.getJSONArray("snippet"); //find path snippet
+
+                                for(int j = 0; j < jsonROW.length(); j++){
+                                    JSONObject snippet = jsonROW.getJSONObject(j);
+                                    String name = snippet.getString("title");     //add video name
+                                    mName.add(name);
+
+                                    JSONArray jsonCOL = snippet.getJSONArray("thumbnails"); //find path thumbnails
+
+                                    for(int k = 0; k < jsonCOL.length(); k++){
+                                        JSONObject thumbnails = jsonCOL.getJSONObject(k);
+                                        JSONArray resolution = thumbnails.getJSONArray("maxres");  //find path maxres
+
+                                        for(int l = 0; l < resolution.length(); l++){
+                                            JSONObject maxres = resolution.getJSONObject(l);
+                                            String JImage = maxres.getString("url");   //add video thumbnails pic.
+                                            mImageUrl.add(JImage);
+                                        }
+                                    }
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "onErrorResponse: Error appear");
+                        error.printStackTrace();
+            }
+        });
+
+        mRequestqueue.add(request);
     }
 
     private void initRecycleView(){
@@ -55,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerv_view);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,mName,mImageUrl,mVideoUrl);
         recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/nikep/recyclerview/YoutubeConfig.java
+++ app/src/main/java/com/example/nikep/recyclerview/YoutubeConfig.java
@@ -4,7 +4,7 @@ public class YoutubeConfig {
     public YoutubeConfig() {
     }
 
-    private static final String API_KEY = "AIzaSyDNeHsNArIXgUK-85v34T4VnggEsJLgJFw";
+    private static final String API_KEY = "AIzaSyDIS87lftBJA1lrSp4umI3t3rr6tfiyByY";
 
     public static String getApiKey() {
         return API_KEY;
